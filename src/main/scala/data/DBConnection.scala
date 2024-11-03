07f2c818c6f150ece4b7e4be5fc9dda629aeaef7
Oleg Bogaty <olegbogaty@gmail.com>
2024-11-03 22:42:11 +0100

    db connection

---
+++ src/main/scala/data/DBConnection.scala
@@ -0,0 +1,29 @@
+package data
+
+import cats.effect.{Resource, Temporal}
+import cats.effect.std.Console
+import conf.DbConfig
+import fs2.io.net.Network
+import natchez.Trace
+import skunk.Session
+
+object DBConnection:
+  def single[F[_] : Temporal : Trace : Network : Console](config: DbConfig): Resource[F, Session[F]] =
+    Session.single(
+      host = config.host.value,
+      port = config.port.value,
+      user = config.user.value,
+      password = Some(config.pass.value),
+      database = config.base.value
+    )
+
+  def pooled[F[_] : Temporal : Trace : Network : Console](config: DbConfig): Resource[F, Resource[F, Session[F]]] =
+    Session.pooled(
+      host = config.host.value,
+      port = config.port.value,
+      user = config.user.value,
+      password = Some(config.pass.value),
+      database = config.base.value,
+      max = 10
+    )
+
