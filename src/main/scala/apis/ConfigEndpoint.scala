4b1cd0c3d2ec13895923574e46579bb1e1c5788a
Oleg Bogaty <olegbogaty@gmail.com>
2024-11-05 00:25:41 +0100

    cleanup

--- src/main/scala/apis/ConfigEndpoint.scala
+++ src/main/scala/apis/ConfigEndpoint.scala
@@ -17,9 +17,6 @@ object ConfigEndpoint:
   //  /config-transfer
   //  An endpoint to manage the processing configuration of transfer requests,
   //  allowing adjustments of parameters like status check frequency and retry limits.
-  //  It should include fields for:
-  //    ∗ retry delay
-  //    ∗ number of attempts
   private val configTransfer
     : PublicEndpoint[ConfigRequest, ConfigError, ConfigResponse, Any] =
     endpoint.post
