42189f9ae2530d1a772e1335e7d767df41c84dbd
Oleg Bogaty <olegbogaty@gmail.com>
2024-11-05 00:26:00 +0100

    add transfer endpoint and service into app dependencies

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,16 +1,23 @@
-import apis.ConfigEndpoint
+import apis.{ConfigEndpoint, TransferEndpoint}
+import cats.Applicative
 import cats.effect.*
 import cats.effect.std.Console
+import cats.syntax.all.*
 import conf.config
 import data.DbConnection
+import data.domain.Transfer
 import fs2.io.net.Network
 import http.HttpServer
 import natchez.Trace
 import natchez.Trace.Implicits.noop
 import repo.{AccountsRepo, TransfersRepo}
-import srvc.TransferConfigService
+import srvc.{TransferConfigService, TransferService}
 
 object App extends IOApp:
+  private def mockService[F[_]: Applicative] =
+    new TransferService[F]: // TODO remove
+      override def transfer(transfer: Transfer): F[Unit] = ().pure[F]
+
   private def makeDependencies[F[_]: Async: Temporal: Trace: Network: Console] =
     for
       config         <- Resource.eval(config)
@@ -21,8 +28,12 @@ object App extends IOApp:
       transferConfigService <- TransferConfigService.makeResource(
         transferConfig
       )
-      configEndpoints <- ConfigEndpoint.makeResource(transferConfigService)
-      server <- HttpServer.makeResource(config.http.server, configEndpoints)
+      transferEndpoint <- TransferEndpoint.makeResource(mockService)
+      configEndpoints  <- ConfigEndpoint.makeResource(transferConfigService)
+      server <- HttpServer.makeResource(
+        config.http.server,
+        transferEndpoint ++ configEndpoints
+      )
     yield server
 
   override def run(args: List[String]): IO[ExitCode] =
