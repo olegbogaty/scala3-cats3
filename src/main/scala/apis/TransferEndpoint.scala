2392a9390089885fbecaf9531aceb1a30a18a0a1
Oleg Bogaty <olegbogaty@gmail.com>
2024-11-05 00:25:31 +0100

    transfer endpoint main with mock service

--- src/main/scala/apis/TransferEndpoint.scala
+++ src/main/scala/apis/TransferEndpoint.scala
@@ -2,65 +2,64 @@ package apis
 
 import apis.model.TransferRequest.transferRequestExample
 import apis.model.{TransferError, TransferRequest, TransferResponse}
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.kernel.Resource
+import cats.effect.{Async, ExitCode, IO, IOApp}
+import cats.implicits.*
+import data.domain.Transfer
+import http.HttpServer
 import io.circe.generic.auto.*
+import srvc.TransferService
+import sttp.tapir.*
 import sttp.tapir.generic.auto.*
 import sttp.tapir.json.circe.*
 import sttp.tapir.server.ServerEndpoint
-import sttp.tapir.server.netty.cats.NettyCatsServer
-import sttp.tapir.swagger.bundle.SwaggerInterpreter
-import sttp.tapir.*
 
 import scala.util.Random
 
 // /enter-transfer
 // An endpoint to accept transfer requests
-// Each request must include essential information such as :
-//  ∗ sender account
-//  ∗ recipient bank code(numeric)
-//  ∗ recipient account (numeric)
-//  ∗ amount
-//  ∗ transaction reference
-
-object EnterTransferEndpoint:
-  val enterTransfer: PublicEndpoint[TransferRequest, TransferError, TransferResponse, Any] =
+object TransferEndpoint:
+  private val enterTransfer
+    : PublicEndpoint[TransferRequest, TransferError, TransferResponse, Any] =
     endpoint.post
       .in("enter-transfer")
       .in(jsonBody[TransferRequest].example(transferRequestExample))
       .out(jsonBody[TransferResponse])
       .errorOut(jsonBody[TransferError])
 
-  def apiEndpoint =
+  private def enterTransferLogic[F[_]: Async](
+    service: TransferService[F]
+  ): ServerEndpoint[Any, F] =
     enterTransfer.serverLogic(request =>
-      IO {
-        Either.cond(
+      Either
+        .cond(
           Random.nextBoolean,
-          TransferResponse("ok"),
+          TransferResponse("transfer success"),
           TransferError("insufficient balance")
         )
-      }
+        .pure[F]
     )
 
-  val docEndpoint: List[ServerEndpoint[Any, IO]] = SwaggerInterpreter()
-    .fromServerEndpoints[IO](List(apiEndpoint), "docs", "1.0.0")
+  def make[F[_]: Async](
+    service: TransferService[F]
+  ): F[List[ServerEndpoint[Any, F]]] =
+    List(enterTransferLogic(service) /*, reviewSettingsLogic(service)*/ )
+      .pure[F]
 
-object EnterTransferEndpointMain extends IOApp:
-  import EnterTransferEndpoint.*
+  def makeResource[F[_]: Async](
+    service: TransferService[F]
+  ): Resource[F, List[ServerEndpoint[Any, F]]] =
+    Resource.eval(make(service))
+
+object TransferEndpointMain extends IOApp:
+  private val mockService = new TransferService[IO]:
+    override def transfer(transfer: Transfer): IO[Unit] = IO.unit
   def run(args: List[String]): IO[ExitCode] =
-    NettyCatsServer
-      .io()
-      .use: server =>
-        for
-          bind <- server
-            .port(8080)
-            .host("localhost")
-            .addEndpoint(apiEndpoint)
-            .addEndpoints(docEndpoint)
-            .start()
-          _ <- IO.println(
-            s"Go to http://localhost:${bind.port}/docs to open SwaggerUI. Press ENTER key to exit."
-          )
-          _ <- IO.readLine
-          _ <- bind.stop()
-        yield bind
+    (for
+      config    <- conf.config[IO]
+      endpoints <- TransferEndpoint.make[IO](mockService)
+      _ <- HttpServer
+        .makeResource[IO](config.http.server, endpoints)
+        .use(_.serve() *> IO.never)
+    yield ())
       .as(ExitCode.Success)
