c1ddbf1455692fbfd9c7fe94877aa79bb6873190
Oleg Bogaty <olegbogaty@gmail.com>
2024-11-13 00:07:52 +0100

    lookup transaction regerence

--- src/main/scala/srvc/PaymentGatewayService.scala
+++ src/main/scala/srvc/PaymentGatewayService.scala
@@ -11,7 +11,7 @@ trait PaymentGatewayService[F[_]]:
 // a method returning either success (transfer accepted and pending) or failure (transfer rejected).
   def enterTransfer(transfer: Transfer): F[Either[TransferErrorResponse, TransferResponse]]
 // a method returning pending, success, or failure, based on the unique transfer identifier.
-  def checkTransferStatus(transfer: Transfer): F[Either[TransferErrorResponse, TransferResponse]]
+  def checkTransferStatus(transactionReference: String): F[Either[TransferErrorResponse, TransferResponse]]
 
 object PaymentGatewayService:
   def make[F[_]: Sync]: F[PaymentGatewayService[F]] =
@@ -19,7 +19,7 @@ object PaymentGatewayService:
       new PaymentGatewayService[F]:
         def enterTransfer(transfer: Transfer): F[Either[TransferErrorResponse, TransferResponse]] =
           Either.cond(Random.nextBoolean, TransferResponse("transfer accepted and pending"), TransferErrorResponse("transfer rejected")).pure[F]
-        def checkTransferStatus(transfer: Transfer): F[Either[TransferErrorResponse, TransferResponse]] =
+        def checkTransferStatus(transactionReference: String): F[Either[TransferErrorResponse, TransferResponse]] =
           val statuses = Transfer.Status.values.map(_.toString)
           Either.cond(Random.nextBoolean, TransferResponse(statuses(Random.nextInt(statuses.length))), TransferErrorResponse("transfer error")).pure[F]
           
