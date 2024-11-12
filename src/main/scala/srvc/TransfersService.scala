5782854d8e1a22fd879c1b90deca1670852397fe
Oleg Bogaty <olegbogaty@gmail.com>
2024-11-12 22:06:46 +0100

    add TransfersService

---
+++ src/main/scala/srvc/TransfersService.scala
@@ -0,0 +1,131 @@
+package srvc
+
+import apis.model.TransferErrorResponse
+import cats.Monad
+import cats.effect.*
+import cats.effect.implicits.*
+import cats.implicits.*
+import cats.syntax.applicative.*
+import data.domain.Transfer.Status
+import data.domain.{Account, Transfer}
+import repo.{AccountsRepo, TransfersRepo}
+import srvc.model.TransferError
+
+trait TransfersService[F[_]]:
+  def transfer(transfer: Transfer): F[Either[TransferError, Transfer]]
+
+object TransfersService:
+  def make[F[_]: Async: Monad](
+    accountsRepo: AccountsRepo[F],
+//                  accountService: AccountService[F],
+    transfersRepo: TransfersRepo[F],
+    paymentGatewayService: PaymentGatewayService[F]
+  ): F[TransfersService[F]] =
+    Sync[F].delay:
+      new TransfersService[F]:
+        private def lookup(accountId: Int): F[Option[Account]] =
+          accountsRepo.select(accountId)
+        private def checkTransfer(
+          account: Account,
+          transfer: Transfer
+        ): Either[String, (Account, Transfer)] =
+          val amount = Either.cond(
+              account.balance >= transfer.amount,
+            (account, transfer),
+            "insufficient amount"
+          )
+          val sameAccount = Either.cond(
+            transfer.accountId != transfer.recipientAccount,
+            (account, transfer),
+            "transfer from and to same account is not allowed"
+          )
+          amount.flatMap(_ => sameAccount.map(identity))
+
+        import io.scalaland.chimney.dsl.*
+        private def enterTransfer(
+          account: Account,
+          transfer: Transfer
+        ): F[Either[TransferError, Transfer]] =
+          for
+            response <- paymentGatewayService.enterTransfer(transfer)
+            result <- response match
+              case Right(response) =>
+                val updatedAccount = account
+                  .into[Account]
+                  .withFieldComputed(_.balance, _.balance - transfer.amount)
+                  .transform
+                for
+                  _ <- accountsRepo.update(updatedAccount)
+                  _ <- transfersRepo.insert(transfer)
+                  _ <- checkTransferStatus(transfer).start
+                yield Right(transfer)
+              case Left(error) =>
+                val updatedTransfer = transfer
+                  .into[Transfer]
+                  .withFieldConst(_.status, Status.FAILURE)
+                  .transform
+                for
+                  _ <- transfersRepo.insert(updatedTransfer)
+                yield Left(TransferError(s"transfer failed: $error"))
+          yield result
+
+        def checkTransferStatus(
+          transfer: Transfer
+        ): F[Transfer.Status] =
+          for
+            status <- paymentGatewayService.checkTransferStatus(transfer)
+            result <- status match
+              case Right(transferStatus) =>
+                Transfer.Status.valueOf(transferStatus.msg) match
+                  case Transfer.Status.FAILURE =>
+                    val updatedTransfer = transfer
+                      .into[Transfer]
+                      .withFieldConst(_.status, Status.FAILURE)
+                      .transform
+                    for
+                      _ <- transfersRepo.update(updatedTransfer)
+                      account <- accountsRepo.select(updatedTransfer.accountId)
+                      updatedAccount = account.map:
+                        _.into[Account]
+                          .withFieldComputed(
+                            _.balance,
+                            _.balance + transfer.amount
+                          )
+                          .transform
+                      _ <- updatedAccount.traverse:
+                        accountsRepo.update
+                    yield Transfer.Status.FAILURE
+                  case Transfer.Status.SUCCESS =>
+                    val updatedTransfer = transfer
+                      .into[Transfer]
+                      .withFieldConst(_.status, Status.SUCCESS)
+                      .transform
+                    for _ <- transfersRepo.update(updatedTransfer)
+                    yield Transfer.Status.SUCCESS
+                  case Transfer.Status.PENDING =>
+                    Transfer.Status.PENDING.pure[F]
+              case Left(errorResponse) =>
+                Transfer.Status.FAILURE.pure[F]
+          yield result
+
+        def transfer(transfer: Transfer): F[Either[TransferError, Transfer]] =
+          for
+            option <- lookup(transfer.accountId)
+            result <- option match
+              case Some(account) =>
+                checkTransfer(account, transfer) match
+                  case Right((account, transfer)) =>
+                    enterTransfer(account, transfer)
+                  case Left(error) =>
+                    Left(TransferError(error)).pure[F]
+              case None => Left(TransferError("account not found")).pure[F]
+          yield result
+
+  def makeResource[F[_]: Async](
+    accountsRepo: AccountsRepo[F],
+//                               accountService: AccountService[F],
+    transferRepo: TransfersRepo[F],
+    paymentGatewayService: PaymentGatewayService[F]
+  ): Resource[F, TransfersService[F]] =
+    Resource.eval:
+      make(accountsRepo, transferRepo, paymentGatewayService)
