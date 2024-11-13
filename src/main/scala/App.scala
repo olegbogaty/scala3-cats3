48728fed763c3e47c2aefcfb9bebf66863f08005
Oleg Bogaty <olegbogaty@gmail.com>
2024-11-13 10:58:22 +0100

    acc service into deps

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,40 +1,41 @@
 import apis.{ConfigEndpoint, TransferEndpoint}
-import cats.Applicative
 import cats.effect.*
 import cats.effect.std.Console
-import cats.syntax.all.*
 import conf.config
 import data.DbConnection
-import data.domain.Transfer
 import fs2.io.net.Network
 import http.HttpServer
 import natchez.Trace
 import natchez.Trace.Implicits.noop
 import repo.{AccountsRepo, TransfersRepo}
-import srvc.{PaymentGatewayService, TransferConfigService, TransfersService}
+import srvc.{AccountService, PaymentGatewayService, TransferConfigService, TransfersService}
 
 object App extends IOApp:
   private def makeDependencies[F[_]: Async: Temporal: Trace: Network: Console] =
     for
-      config         <- Resource.eval(config)
-      session        <- DbConnection.single(config.db)
-      accountRepo    <- AccountsRepo.makeResource(session)
-      transferRepo   <- TransfersRepo.makeResource(session)
-      transferConfig <- Resource.eval(Ref[F].of(config.tc))
+      config                <- Resource.eval(config)
+      session               <- DbConnection.single(config.db)
+      accountRepo           <- AccountsRepo.makeResource(session)
+      transferRepo          <- TransfersRepo.makeResource(session)
+      transferConfig        <- Resource.eval(Ref[F].of(config.tc))
       paymentGatewayService <- PaymentGatewayService.makeResource
       transferConfigService <- TransferConfigService.makeResource(
         transferConfig
       )
-      transfersService <- TransfersService.makeResource(accountRepo, transferRepo, paymentGatewayService)
-      transferEndpoint <- TransferEndpoint.makeResource(transfersService)
-      configEndpoints  <- ConfigEndpoint.makeResource(transferConfigService)
+      accountService <- AccountService.makeResource(accountRepo)
+      transfersService <- TransfersService.makeResource(
+        accountService,
+        transferRepo,
+        paymentGatewayService
+      )
+      transferEndpoints <- TransferEndpoint.makeResource(transfersService)
+      configEndpoints   <- ConfigEndpoint.makeResource(transferConfigService)
       server <- HttpServer.makeResource(
         config.http.server,
-        transferEndpoint ++ configEndpoints
+        transferEndpoints ++ configEndpoints
       )
     yield server
 
   override def run(args: List[String]): IO[ExitCode] =
-    makeDependencies[IO]
-      .useForever
+    makeDependencies[IO].useForever
       .as(ExitCode.Success)
--- src/main/scala/srvc/TransfersService.scala
+++ src/main/scala/srvc/TransfersService.scala
@@ -19,15 +19,12 @@ trait TransfersService[F[_]]:
 
 object TransfersService:
   def make[F[_]: Async: Monad](
-    accountsRepo: AccountsRepo[F],
-//                  accountService: AccountService[F],
+    accountService: AccountService[F],
     transfersRepo: TransfersRepo[F],
     paymentGatewayService: PaymentGatewayService[F]
   ): F[TransfersService[F]] =
     Sync[F].delay:
       new TransfersService[F]:
-        private def lookup(accountId: Int): F[Option[Account]] =
-          accountsRepo.select(accountId)
         private def checkTransfer(
           account: Account,
           transfer: Transfer
@@ -53,12 +50,8 @@ object TransfersService:
             response <- paymentGatewayService.enterTransfer(transfer)
             result <- response match
               case Right(response) =>
-                val updatedAccount = account
-                  .into[Account]
-                  .withFieldComputed(_.balance, _.balance - transfer.amount)
-                  .transform
                 for
-                  _ <- accountsRepo.update(updatedAccount)
+                  _ <- accountService.enterWithdrawal(account, transfer.amount)
                   _ <- transfersRepo.insert(transfer)
                   _ <- checkTransferStatus(transfer).start
                 yield Right(transfer)
@@ -68,7 +61,7 @@ object TransfersService:
                   .withFieldConst(_.status, Status.FAILURE)
                   .transform
                 for _ <- transfersRepo.insert(updatedTransfer)
-                yield Left(TransferError(s"transfer failed: $error"))
+                yield Left(TransferError(s"transfer failed: ${error.msg}"))
           yield result
 
         override def checkTransferStatus(
@@ -83,6 +76,26 @@ object TransfersService:
                 case status => status.pure[F]
           yield result
 
+        private def handleTransferFailure(transfer: Transfer): F[Transfer.Status] =
+          val updatedTransfer = transfer
+            .into[Transfer]
+            .withFieldConst(_.status, Status.FAILURE)
+            .transform
+          for
+            _ <- transfersRepo.update(updatedTransfer)
+            account <- accountService.lookup(updatedTransfer.accountId)
+            _ <- account.traverse:
+              accountService.enterWithdrawal(_, -transfer.amount)
+          yield Transfer.Status.FAILURE
+
+        private def handleTransferSuccess(transfer: Transfer): F[Transfer.Status] =
+          val updatedTransfer = transfer
+            .into[Transfer]
+            .withFieldConst(_.status, Status.SUCCESS)
+            .transform
+          for _ <- transfersRepo.update(updatedTransfer)
+            yield Transfer.Status.SUCCESS
+
         private def checkTransferStatus(
           transfer: Transfer
         ): F[Transfer.Status] =
@@ -94,30 +107,9 @@ object TransfersService:
               case Right(transferStatus) =>
                 Transfer.Status.valueOf(transferStatus.msg) match
                   case Transfer.Status.FAILURE =>
-                    val updatedTransfer = transfer
-                      .into[Transfer]
-                      .withFieldConst(_.status, Status.FAILURE)
-                      .transform
-                    for
-                      _       <- transfersRepo.update(updatedTransfer)
-                      account <- accountsRepo.select(updatedTransfer.accountId)
-                      updatedAccount = account.map:
-                        _.into[Account]
-                          .withFieldComputed(
-                            _.balance,
-                            _.balance + transfer.amount
-                          )
-                          .transform
-                      _ <- updatedAccount.traverse:
-                        accountsRepo.update
-                    yield Transfer.Status.FAILURE
+                    handleTransferFailure(transfer)
                   case Transfer.Status.SUCCESS =>
-                    val updatedTransfer = transfer
-                      .into[Transfer]
-                      .withFieldConst(_.status, Status.SUCCESS)
-                      .transform
-                    for _ <- transfersRepo.update(updatedTransfer)
-                    yield Transfer.Status.SUCCESS
+                    handleTransferSuccess(transfer)
                   case Transfer.Status.PENDING =>
                     Transfer.Status.PENDING.pure[F]
               case Left(errorResponse) =>
@@ -126,7 +118,7 @@ object TransfersService:
 
         def transfer(transfer: Transfer): F[Either[TransferError, Transfer]] =
           for
-            option <- lookup(transfer.accountId)
+            option <- accountService.lookup(transfer.accountId)
             result <- option match
               case Some(account) =>
                 checkTransfer(account, transfer) match
@@ -138,10 +130,9 @@ object TransfersService:
           yield result
 
   def makeResource[F[_]: Async](
-    accountsRepo: AccountsRepo[F],
-//                               accountService: AccountService[F],
+    accountService: AccountService[F],
     transferRepo: TransfersRepo[F],
     paymentGatewayService: PaymentGatewayService[F]
   ): Resource[F, TransfersService[F]] =
     Resource.eval:
-      make(accountsRepo, transferRepo, paymentGatewayService)
+      make(accountService, transferRepo, paymentGatewayService)
