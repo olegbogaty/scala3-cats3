2ec25c86800deb3287b5dfdb531cf137357e51aa
Oleg Bogaty <olegbogaty@gmail.com>
2024-11-13 20:34:53 +0100

    fix warnings

--- src/test/scala/conf/EnvHacker.scala
+++ src/test/scala/conf/EnvHacker.scala
@@ -5,28 +5,36 @@ import java.util.Map as JavaMap
 
 // https://gist.github.com/jaytaylor/770bc416f0dd5954cf0f
 trait EnvHacker { // TODO remove or find another solution
-  /**
-   * Portable method for setting env vars on both *nix and Windows.
-   * @see http://stackoverflow.com/a/7201825/293064
-   */
-  def setEnv(newEnv: JavaMap[String, String]): Unit = {
+  /** Portable method for setting env vars on both *nix and Windows.
+    * @see
+    *   http://stackoverflow.com/a/7201825/293064
+    */
+  def setEnv(newEnv: JavaMap[String, String]): Unit =
     try {
-      val processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment")
-      val theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment")
+      val processEnvironmentClass =
+        Class.forName("java.lang.ProcessEnvironment")
+      val theEnvironmentField =
+        processEnvironmentClass.getDeclaredField("theEnvironment")
       theEnvironmentField.setAccessible(true)
-      val env = theEnvironmentField.get(null).asInstanceOf[JavaMap[String, String]]
+      val env =
+        theEnvironmentField.get(null).asInstanceOf[JavaMap[String, String]]
       env.putAll(newEnv)
-      val theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment")
+      val theCaseInsensitiveEnvironmentField =
+        processEnvironmentClass.getDeclaredField(
+          "theCaseInsensitiveEnvironment"
+        )
       theCaseInsensitiveEnvironmentField.setAccessible(true)
-      val cienv = theCaseInsensitiveEnvironmentField.get(null).asInstanceOf[JavaMap[String, String]]
+      val cienv = theCaseInsensitiveEnvironmentField
+        .get(null)
+        .asInstanceOf[JavaMap[String, String]]
       cienv.putAll(newEnv)
     } catch {
       case e: NoSuchFieldException =>
         try {
-          val classes = classOf[Collections].getDeclaredClasses()
-          val env = System.getenv()
+          val classes = classOf[Collections].getDeclaredClasses
+          val env     = System.getenv()
           for (cl <- classes) {
-            if (cl.getName() == "java.util.Collections$UnmodifiableMap") {
+            if (cl.getName == "java.util.Collections$UnmodifiableMap") {
               val field = cl.getDeclaredField("m")
               field.setAccessible(true)
               val obj = field.get(env)
@@ -41,5 +49,4 @@ trait EnvHacker { // TODO remove or find another solution
 
       case e1: Exception => e1.printStackTrace()
     }
-  }
 }
