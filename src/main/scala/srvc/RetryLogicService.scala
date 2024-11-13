25fe624a7a8fee7903cb988e736638d91ea17fe8
Oleg Bogaty <olegbogaty@gmail.com>
2024-11-13 11:01:13 +0100

    retry logic service // todo

--- src/main/scala/srvc/RetryLogicService.scala
+++ src/main/scala/srvc/RetryLogicService.scala
@@ -1,5 +1,19 @@
 package srvc
 
-trait RetryLogicService[F[_]]:
-  def retry[A](fa: F[A]): F[Unit]
+import cats.effect.Temporal
+import cats.syntax.all.*
 
+import scala.concurrent.duration.FiniteDuration
+
+trait RetryLogicService[F[_]: Temporal]:
+  def retry[A](fa: F[A], delay: FiniteDuration, tries: Int): F[A] =
+    if (tries <= 1)
+      fa
+    else
+      Temporal[F].sleep(delay) *> retry(fa, delay, tries - 1)
+
+  def retry[A, B](fa: F[A], delay: FiniteDuration, tries: Int, onSuccess: F[A] => F[B], onFailure: F[A] => F[B]): F[A] =
+    if (tries <= 1)
+      fa
+    else
+      Temporal[F].sleep(delay) *> retry(fa, delay, tries - 1)
