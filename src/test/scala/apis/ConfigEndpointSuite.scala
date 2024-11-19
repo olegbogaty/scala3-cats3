9b77ccf42431853ad00653f2c38ebcb2863ce977
Oleg Bogaty <olegbogaty@gmail.com>
2024-11-19 22:19:40 +0100

    FIX ConfigEndpointSuite

--- src/test/scala/apis/ConfigEndpointSuite.scala
+++ src/test/scala/apis/ConfigEndpointSuite.scala
@@ -2,10 +2,15 @@ package apis
 
 import apis.model.{ConfigRequest, ConfigResponse}
 import cats.effect.*
+import cats.effect.kernel.Resource
+import cats.effect.std.Console
 import conf.Config
 import conf.Config.TransferConfig
+import eu.timepit.refined.types.net.UserPortNumber
+import http.HttpServer
 import io.circe.generic.auto.*
 import munit.CatsEffectSuite
+import munit.catseffect.IOFixture
 import srvc.TransferConfigService
 import sttp.client3.*
 import sttp.client3.circe.*
@@ -13,32 +18,39 @@ import sttp.model.StatusCode
 
 class ConfigEndpointSuite extends CatsEffectSuite:
 
+  private val httpServerPort = UserPortNumber.unsafeFrom(
+    8888
+  ) // CANNOT USE FIXTURE FOR SUITE, NEED TO CHANGE
   private val configRequest = ConfigRequest(tries = 5, delay = 5)
   private val initialConfig = TransferConfig.fromRequest(configRequest)
 
-  def withServer[T](service: TransferConfigService[IO])(
+  def makeServer[F[_]: Async: Console]: Resource[F, HttpServer[F]] =
+    for
+      config  <- Config.makeResource
+      service <- TransferConfigService.makeResource(initialConfig)
+      routes  <- ConfigEndpoint.makeResource(service)
+      server <- http.HttpServer.makeResource(
+        config.http.server.copy(port = httpServerPort),
+        routes
+      )
+    yield server
+
+  val httpServer: IOFixture[HttpServer[IO]] = ResourceSuiteLocalFixture(
+    "http-server",
+    makeServer[IO]
+  )
+
+  override def munitFixtures: Seq[IOFixture[HttpServer[IO]]] = List(httpServer)
+
+  def withServer[T](
     test: SttpBackend[Identity, Any] => IO[T]
   ): IO[Unit] =
-    val serverResource =
-      for
-        config <- Config.makeResource[IO]
-        routes <- ConfigEndpoint.makeResource(service)
-        server <- http.HttpServer.makeResource(config.http.server, routes)
-      yield server
-    serverResource.use { server =>
-      for
-        binding <- server.serve()
-        _       <- test(HttpURLConnectionBackend())
-        _       <- IO.blocking(binding).guarantee(binding.stop())
-      yield ()
-    }
+    test(HttpURLConnectionBackend()).void
 
   test("GET /config-transfer should return the current configuration"):
-    for
-      service <- TransferConfigService.make[IO](initialConfig)
-      _ <- withServer(service) { backend =>
+    for _ <- withServer { backend =>
         val res = basicRequest
-          .get(uri"http://localhost:8080/config-transfer")
+          .get(uri"http://localhost:$httpServerPort/config-transfer")
           .response(asJson[ConfigResponse])
           .send(backend)
         IO {
@@ -55,16 +67,14 @@ class ConfigEndpointSuite extends CatsEffectSuite:
 
   test("POST /config-transfer should update the configuration"):
     val newConfig = ConfigRequest(tries = 10, delay = 5)
-    for
-      service <- TransferConfigService.make[IO](initialConfig)
-      _ <- withServer(service) { backend =>
+    for _ <- withServer { backend =>
         val updateResponse = basicRequest
-          .post(uri"http://localhost:8080/config-transfer")
+          .post(uri"http://localhost:$httpServerPort/config-transfer")
           .body(newConfig)
           .send(backend)
 
         val fetchResponse = basicRequest
-          .get(uri"http://localhost:8080/config-transfer")
+          .get(uri"http://localhost:$httpServerPort/config-transfer")
           .response(asJson[ConfigResponse])
           .send(backend)
 
